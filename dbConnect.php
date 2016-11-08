7a6b1276aeba42be455a1eec1e1cddaf8a8b4560
FebriPratama <febri.pr@students.amikom.ac.id>
2016-11-08 11:04:05 +0700

    init

--- dbConnect.php
+++ dbConnect.php
@@ -2,9 +2,9 @@
 /* author: Ridwan website: http://blogciqwan.com/ */ 
 //Defining Constants 
 define('HOST','localhost'); 
-define('USER','root'); 
-define('PASS',''); 
-define('DB','dbcourse'); 
+define('USER','admin_adit'); 
+define('PASS','silahkanmasuk123'); 
+define('DB','admin_adit'); 
 //Connecting to Database 
 $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect'); 
 ?>
